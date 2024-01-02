d02d9b5bad87ce2e12b7bc8acfc9f9d431121526
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-01-02 16:36:12 +0900

    commit

---
+++ src/main/java/com/eventstorming/domain/test.java
@@ -0,0 +1,6 @@
+forEach: Aggregate
+representativeFor: Aggregate
+fileName: {{namePascalCase}}test.java
+path: {{boundedContext.name}}/{{{options.packagePath}}}/domain
+--- 
+package {{options.package}}.domain;
